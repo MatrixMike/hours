f41abcb0b28f140501d4a20b34eebfcd76135627
Mike J Hewitt <mikeh@electroteach.com>
2017-10-19 14:12:57 +1100

    how does this work?

--- wtf1.hs
+++ wtf1.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 main :: IO ()
 --main = print $ (!!5) $ scanl (*) 1 [1..]
-main = print $ (!!4) $ scanl (*) 1 [1..]
+main = print $ (!! 4) $ scanl (*) 1 [1 ..]
